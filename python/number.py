f8b2918ed9105fe222b48badbb6496ba5f9ce235
Sanika Pawar <pawarsanika3130@gmail.com>
2026-06-11 10:13:34 +0530

    Add number.py to calculate statistics of a list

---
+++ python/number.py
@@ -0,0 +1,12 @@
+# python/numbers.py
+
+numbers = [12, 45, 78, 23, 90, 34, 67, 11, 56, 88]
+
+highest = max(numbers)
+lowest = min(numbers)
+average = sum(numbers) / len(numbers)
+
+print("Numbers:", numbers)
+print("Highest:", highest)
+print("Lowest:", lowest)
+print("Average:", average)
